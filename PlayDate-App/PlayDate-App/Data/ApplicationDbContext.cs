5b686bb2ce09f6f04e133b41aa530b5b330f1567
larutow <lucas.rutowski@gmail.com>
2020-12-03 13:26:02 -0600

    updated Kid model, added properties to Parent model

--- PlayDate-App/PlayDate-App/Data/ApplicationDbContext.cs
+++ PlayDate-App/PlayDate-App/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PlayDate_App.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,8 @@ namespace PlayDate_App.Data
             : base(options)
         {
         }
+
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<Kid> Kids { get; set; }
     }
 }
--- PlayDate-App/PlayDate-App/Models/Kid.cs
+++ PlayDate-App/PlayDate-App/Models/Kid.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,19 @@ namespace PlayDate_App.Models
 {
     public class Kid
     {
+        [Key]
+        public int KidId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        public bool Immunized { get; set; }
+        public bool WearsMask { get; set; }
+        public bool SpecialNeeds { get; set; }
+        public string Notes { get; set; }
+        
+        [ForeignKey("Parent")]
+        public int ParentId { get; set; }
+
     }
 }
