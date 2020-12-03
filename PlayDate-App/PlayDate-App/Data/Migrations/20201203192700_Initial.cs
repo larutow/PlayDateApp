75d92ec7156e0d28d03d97b997bc240edf644a36
larutow <lucas.rutowski@gmail.com>
2020-12-03 13:34:45 -0600

    added Contracts/IRepositoryBase

---
+++ PlayDate-App/PlayDate-App/Data/Migrations/20201203192700_Initial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlayDate_App.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kids",
+                columns: table => new
+                {
+                    KidId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    Immunized = table.Column<bool>(nullable: false),
+                    WearsMask = table.Column<bool>(nullable: false),
+                    SpecialNeeds = table.Column<bool>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    ParentId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kids", x => x.KidId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parents",
+                columns: table => new
+                {
+                    ParentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: true),
+                    LocationZip = table.Column<int>(nullable: false),
+                    ImagePath = table.Column<string>(nullable: true),
+                    EmailAddress = table.Column<string>(nullable: true),
+                    Lat = table.Column<double>(nullable: false),
+                    Lng = table.Column<double>(nullable: false),
+                    ThumbsUp = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parents", x => x.ParentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kids");
+
+            migrationBuilder.DropTable(
+                name: "Parents");
+        }
+    }
+}
