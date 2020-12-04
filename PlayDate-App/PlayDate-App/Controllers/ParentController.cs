b4d65fbb9de4b5d0c9ecb4f80a8388e652a08b29
larutow <lucas.rutowski@gmail.com>
2020-12-04 10:29:42 -0600

    added parent index & create controller logic, added parent create view

---
+++ PlayDate-App/PlayDate-App/Controllers/ParentController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlayDate_App.Contracts;
+using PlayDate_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PlayDate_App.Controllers
+{
+    public class ParentController : Controller
+    {
+        private IRepositoryWrapper _repo;
+        public ParentController(IRepositoryWrapper repo)
+        {
+            _repo = repo;
+        }
+        // GET: ParentController
+        public ActionResult Index()
+        {
+            var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var parent = _repo.Parent.FindByCondition(p => p.IdentityUserId == identityUserId).FirstOrDefault();
+            if(parent == null)
+            {
+                return RedirectToAction("Create");
+            }
+
+            //index view logic - home screen
+
+            return View();
+        }
+
+        // GET: ParentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ParentController/Create
+        public ActionResult Create()
+        {
+            Parent parent = new Parent();
+
+            return View(parent);
+        }
+
+        // POST: ParentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ParentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ParentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ParentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ParentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
