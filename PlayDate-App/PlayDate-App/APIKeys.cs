a22ce05cfaaf8c8987fc579c30057512107fdd31
Nicole Mielnicki <bruce.macdonald.code@gmail.com>
2020-12-04 13:29:03 -0700

    Added Event Class w/Properties

---
+++ PlayDate-App/PlayDate-App/APIKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayDate_App
+{
+    public static class APIKeys
+    {
+        public static string GoogleApiKey = "AIzaSyCYTeKQ_TFp9FbW8zgukK1ZABSoVxvnpNU";
+    }
+}
---
+++ PlayDate-App/PlayDate-App/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayDate_App.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+
+        [ForeignKey("LocationId")]
+        public string LocationId { get; set; }
+        public Location Location { get; set; }
+        public DateTime TimeAndDate { get; set; }
+        public bool ConfirmedEvent { get; set; }
+        public bool IsPrivate { get; set; }
+        public int Capacity { get; set; }
+
+
+    }
+}
