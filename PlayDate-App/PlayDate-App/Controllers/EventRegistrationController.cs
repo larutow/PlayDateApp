537cb7f3b57d8bc36678d7459d18d8e76b6acb18
Clint <crob2099@gmail.com>
2020-12-09 11:22:06 -0500

    Modified Event controller to display Address name and updated views

---
+++ PlayDate-App/PlayDate-App/Controllers/EventRegistrationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlayDate_App.Contracts;
+using PlayDate_App.Models;
+
+namespace PlayDate_App.Controllers
+{
+    public class EventRegistrationController : Controller
+    {
+        private IRepositoryWrapper _repo;
+
+        public EventRegistrationController(IRepositoryWrapper repo)
+        {
+            _repo = repo;
+        }
+
+      
+    }
+}
